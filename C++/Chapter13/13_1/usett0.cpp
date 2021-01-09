13ca5850aaf1d9ce370efdef6a93667365c93fac
fengxuewei-dev <15235170935@163.com>
2021-01-09 15:31:46 +0800

    new

--- C++/Chapter13/13_1/usett0.cpp
+++ C++/Chapter13/13_1/usett0.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include "tabtenn0.h"
 
+void Show(const TableTennisPlayer & rt)
+{
+    using std::cout;
+    cout << "Name: ";
+    rt.Name();  cout << "\n Table:" ; 
+    if(rt.HasTable())
+        cout << "yes\n";
+    else
+    {
+        cout << "no\n";
+    }
+    
+}
+
 int main(void){
     using std::cout;
     using std::endl;
@@ -39,5 +53,18 @@ int main(void){
     cout << "Name: ";
     rplayer2.Name();
     cout << "; Rating : " << rplayer2.Rating() << endl;
+
+
+    cout << "派生类和基类之间的特殊关系:" << endl;
+    TableTennisPlayer & rt = rplayer1;          // 基类引用指向派生类对象
+    TableTennisPlayer * pt = &rplayer1;         // 基类指针指向派生类对象地址
+
+    rt.Name();
+    cout << endl;
+    pt->Name();
+    cout << endl;
+
+    Show(player1);      // 基类
+    Show(rplayer1);     // 派生类
     return 0;
 }
